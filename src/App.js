27bfe6877b0a7a8b7b3819f9b27ed44f37d45d51
hyujisf <canqalloid.me@gmail.com>
2022-09-19 18:14:07 +0700

    create testing file for button

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "assets/scss/style.scss"
+
+function App() {
+	return (
+		<div className='App'>
+			somethings
+			<div className=''>somethings</div>
+		</div>
+	)
+}
+
+export default App
---
+++ src/elements/Button/index.test.js
@@ -0,0 +1,20 @@
+import React from "react"
+import { render } from "@testing-library/react"
+import Button from "./index"
+
+test("Should not allowed to click button if isDisabled is used", () => {
+	const { container } = render(<Button isDisabled></Button>)
+	expect(container.querySelector("span.disabled")).toBeInTheDocument()
+})
+
+test("Should render loading/spinner", () => {
+	const { container, getByText } = render(<Button isLoading></Button>)
+	expect(getByText(/loading/i)).toBeInTheDocument()
+
+	expect(container.querySelector("span")).toBeInTheDocument()
+})
+
+test("Should render <a> tag", () => {
+	const { container } = render(<Button type='link' isExternal></Button>)
+	expect(container.querySelector("a")).toBeInTheDocument()
+})
